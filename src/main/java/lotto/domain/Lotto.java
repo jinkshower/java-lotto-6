2b941ede2977913763cf8c2ed8248bfee67e48a8
jinkshower <askthem@naver.com>
2023-11-28 11:00:26 +0900

    feat(domain): 로또 생성 기능 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,52 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException();
+        }
+        if (Set.copyOf(numbers).size() != numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+        if (isOutOfRange(numbers)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isOutOfRange(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(i -> i < MIN_LOTTO_NUMBER || i > MAX_LOTTO_NUMBER);
+    }
+
+    public List<Integer> sortedLotto() {
+        return numbers.stream()
+                .sorted()
+                .toList();
+    }
+
+    public int countMatch(Lotto otherLotto) {
+        return (int) numbers.stream()
+                .filter(otherLotto::contains)
+                .count();
+    }
+
+    public boolean contains(int number) {
+        return this.numbers.contains(number);
+    }
+
+}
---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,60 @@
+package lotto.domain;
+
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class LottoTest {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 1~45가 아닌 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRangeNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("오름차순으로 정렬된 로또를 얻을 수 있다")
+    @Test
+    void getSortedLotto() {
+        Lotto lotto = new Lotto(List.of(5, 45, 16, 2, 1, 8));
+
+        assertThat(lotto.sortedLotto()).isEqualTo(List.of(1, 2, 5, 8, 16, 45));
+    }
+
+    @DisplayName("로또는 다른 로또와의 일치 개수를 셀 수 있다")
+    @Test
+    void getMatchCount() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto otherLotto = new Lotto(List.of(16, 2, 3, 4, 5, 45));
+
+        assertThat(lotto.countMatch(otherLotto)).isEqualTo(4);
+    }
+
+    @DisplayName("정상적으로 로또를 생성할 수 있다")
+    @Test
+    void createProperLotto() {
+        assertDoesNotThrow(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+    }
+
+}
